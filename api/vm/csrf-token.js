45328d372038d873b5392ea0d7823880fc77a670
Cyabanz <118572258+Cyabanz@users.noreply.github.com>
2025-07-01 18:21:27 -0500

    Update csrf-token.js

--- api/vm/csrf-token.js
+++ api/vm/csrf-token.js
@@ -4,18 +4,16 @@ import cookie from "cookie";
 const tokens = new Tokens();
 
 export default function handler(req, res) {
-  // Generate a new CSRF secret and token on request
   const secret = tokens.secretSync();
   const token = tokens.create(secret);
 
-  // Set CSRF secret as an HTTP-only cookie
   res.setHeader(
     "Set-Cookie",
     cookie.serialize("csrfSecret", secret, {
       httpOnly: true,
       sameSite: "lax",
       path: "/",
-      maxAge: 60 * 60 * 24, // 1 day
+      maxAge: 60 * 60 * 24,
       secure: process.env.NODE_ENV === "production",
     })
   );
